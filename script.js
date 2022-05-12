bcf02eb1778a4e26e8071e8c4c1b46dcd8ec8218
AlexOnWax <alexandre.barbet@hotmail.com>
2022-05-12 15:52:54 +0200

    mis en place des liens et maj general

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const cont = document.querySelector(".flex");
 const header = document.querySelector(".logo-title-header")
 const arrowR = document.querySelector("#arrow")
 const arrowL = document.querySelector("#arrowleft")
+const body = document.querySelector('body')
+const navDots = document.querySelectorAll(".navdot")
+
+
+let scrollSize = (body.clientWidth);
+
+
+for (dot of navDots) {
+  dot.addEventListener('click',function() {
+    console.log("okidoki");
+    dot.style.backgroundColor="red";
+  })
+}
+
+
 
 cont.addEventListener("wheel",(x) => {
 // evt.preventDefault();
@@ -14,11 +29,11 @@ cont.addEventListener("wheel",(x) => {
 
 arrowR.addEventListener('click',() =>{
     
-    cont.scrollLeft +=1600;
+    cont.scrollLeft +=scrollSize;
 
 });
 arrowL.addEventListener('click',() =>{
     
-    cont.scrollLeft -=1600;
+    cont.scrollLeft -=scrollSize;
 
 });
