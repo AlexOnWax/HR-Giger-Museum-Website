4dbb3ac27c6d711fc20ee19a6afd4a71fe16af63
AlexOnWax <alexandre.barbet@hotmail.com>
2022-06-24 17:07:53 +0200

    ajout de composer et travail sur php formulaire

--- formulaire_inscription-newletter.php
+++ formulaire_inscription-newletter.php
@@ -4,6 +4,67 @@ $texte = $_POST['email'] ;
 echo $texte;
 
 
+		
+
+		$texte = $_POST['email'] ;
+		echo $texte;
+		$email = $_POST['email'] ;
+		
+
+		if (isset($_POST["email"])){
+		if (empty($email)){
+		    echo "Value missing";
+		}else if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		    echo "ok";
+		
+		try{
+		$conn = new PDO("mysql:host=$servername;dbname=$dbName",$username,$password);
+		$conn->setAttribute(PDO::ATTR_ERMODE, PDO::ERRMODE_EXCEPTION);
+		
+
+		 $conn = $sql->prepare("INSERT INTO 
+		 Newsletter(Mail)
+		 VALUES($email)");
+		 $conn->execute();
+		
+
+		
+
+		
+
+		}
+		
+
+		
+
+		
+
+		
+
+		}else{
+		    echo "Pas le bon Format";
+		}}
+		
+
+		
+
+		
+
+		
+
+		
+
+		catch(PDOException $e){
+		    echo "Erreur :" . $e->getMessage();
+		};
+		
+
+		
+
+		
+
+		
+
 
 
 
