43157fb7f0fb2126f0527d385e2af0f767d383fe
AlexOnWax <alexandre.barbet@hotmail.com>
2022-06-27 11:43:21 +0200

    changement des noms dans gitignore

--- formulaire_inscription-newletter.php
+++ formulaire_inscription-newletter.php
@@ -1,31 +1,37 @@
 <?php
-	
-$email = $_POST['email'] ;
+	require __DIR__ .'/vendor/autoload.php';
+	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+	$dotenv->load();
 
-$servername='';
-$dbName='';
-$user='';
-$pass='';
 
-if(!empty($_POST['email']&& filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-	
+
+$servername=$_ENV['servername'];
+$dbName=$_ENV['dbName'];
+$user=$_ENV['username'];
+$pass=$_ENV['password'];
+
+
+
+if(!empty($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) == true){
+	$email = $_POST['email'];
+
 	try{
-		$dbnl = new PDO("mysql:host=$servername;dbname=$dbName",$username,$password);
-		$dbnl->setAttribute(PDO::ATTR_ERMODE, PDO::ERRMODE_EXCEPTION);
-		$sql=$dbnl->prepare("INSERT INTO Newsletter(Mail) VALUES(:email);";)
-		$sql->bindParam(':email', $_POST['email'],PDO::PARAM_STR);
+		$dbnl = new PDO('mysql:host='.$servername.';dbname='.$dbName, $user, $pass);
+		$dbnl->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql=$dbnl->prepare("INSERT INTO Mail(email) VALUES(:email)");
+		$sql->bindParam(':email', $_POST['email'],PDO::PARAM_STR);//Ajoute securité précise le type de donné attendu
 		$sql->execute();	
 	}
 catch(PDOException $e){
-	$dbnl->rollBack();
-echo "Erreur :" . $e->getMessage();
-		};
+	
+echo "Erreur :" . $e;
+		}
+$dbnl=null;
 }
 ?>
 
 
 
-<?php
 
 
 
