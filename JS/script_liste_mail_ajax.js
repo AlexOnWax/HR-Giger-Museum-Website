8bd13141a02f691b638d6438ebac21c4bc73375a
AlexOnWax <alexandre.barbet@hotmail.com>
2022-07-08 16:28:51 +0200

    wip

--- JS/script_liste_mail_ajax.js
+++ JS/script_liste_mail_ajax.js
@@ -19,98 +19,72 @@ window.addEventListener("DOMContentLoaded", () => {
         td[2].setAttribute("data-value", `${data[i].idNewsletter}`);
         tbody.appendChild(clone);
 
-        //boutons supprimer 
+        //boutons supprimer
         const tdSuppr = document.querySelectorAll(".btn_suppr");
         tdSuppr.forEach((elem) => {
           elem.addEventListener("click", (e) => {
             const elementClicked = e.currentTarget;
             elem.parentNode.remove();
             const valueToDelete = elementClicked.dataset.value;
-        //fetch de la requete pour la suppression en sql
+            //fetch de la requete pour la suppression en sql
             fetch(`delete_mail-list.php?idToSuppr=${valueToDelete}`, {
               method: "GET",
             });
           });
         });
       }
-      const btnOrderMail= document.querySelector("#order_mail");
-      btnOrderMail.addEventListener('click', () => {
-   trieAlpha(0);
-        
-      })
-     
     });
 });
+const btnOrderMail = document.querySelector("#order_mail");
+let moduloMail = 0;
 
-const btnOrderDate = document.getElementById("order_date");
-btnOrderDate.addEventListener('click', function() {
-trieAlpha(1);
+btnOrderMail.addEventListener("click", () => {
+  moduloMail++;
+                                            // a faire !!!! ajouter modulo pour switcher!!
+    trieAlpha(0);
   
-})
+  const btnOrderDate = document.getElementById("order_date");
+  btnOrderDate.addEventListener("click", function () {
 
+    trieAlpha(1);
+  });
 
+  function trieAlpha(a) {
+    let lignes = document.querySelectorAll(".lignes");
+    const tBody = document.querySelector("tbody");
+    for (let i = 0; i < lignes.length; i++) {
+      let email = lignes[i].querySelector(`.td${a}`).dataset.value;
 
-
-
-
-function trieAlpha(a) {
-  let lignes = document.querySelectorAll(".lignes");
-  const tBody= document.querySelector('tbody');
-  for (let i = 0; i < lignes.length; i++) {
-   
-    let email = lignes[i].querySelector(`.td${a}`).dataset.value;
-    
-    for (let k = i + 1; k < lignes.length ; k++) {
-      let emailK = lignes[k].querySelector(`.td${a}`).dataset.value;
-      if (email > emailK) {
+      for (let k = i + 1; k < lignes.length; k++) {
+        let emailK = lignes[k].querySelector(`.td${a}`).dataset.value;
         
-        tBody.insertBefore(lignes[k],lignes[i]);
-        lignes = document.querySelectorAll(".lignes");
-        email = lignes[i].querySelector(`.td${a}`).dataset.value;
-       
+          if (email > emailK) {
+            tBody.insertBefore(lignes[k], lignes[i]);
+            lignes = document.querySelectorAll(".lignes");
+            email = lignes[i].querySelector(`.td${a}`).dataset.value;
+          }
+        
       }
     }
   }
-}
+  function trieAlpha2(a) {
+    let lignes = document.querySelectorAll(".lignes");
+    const tBody = document.querySelector("tbody");
+    for (let i = 0; i < lignes.length; i++) {
+      let email = lignes[i].querySelector(`.td${a}`).dataset.value;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function trieAlpha() {
-//   let lignes = document.querySelectorAll(".lignes");
-//   const tBody= document.querySelector('tbody');
-//   for (let i = 0; i < lignes.length; i++) {
-   
-//     let email = lignes[i].querySelector(".td0").dataset.value;
-//     for (let k = i + 1; k < lignes.length ; k++) {
-//       let emailK = lignes[k].querySelector(".td0").dataset.value;
-//       if (email > emailK) {
+      for (let k = i + 1; k < lignes.length; k++) {
+        let emailK = lignes[k].querySelector(`.td${a}`).dataset.value;
         
-//         tBody.insertBefore(lignes[k],lignes[i]);
-//         lignes = document.querySelectorAll(".lignes");
-//         email = lignes[i].querySelector('.td0').dataset.value;
-       
-//       }
-//     }
-//   }
-// }
+          if (email < emailK) {
+            tBody.insertBefore(lignes[k], lignes[i]);
+            lignes = document.querySelectorAll(".lignes");
+            email = lignes[i].querySelector(`.td${a}`).dataset.value;
+          }
+        
+      }
+    }
+  }
 
-
-
-
-
-
-
+  
+});
