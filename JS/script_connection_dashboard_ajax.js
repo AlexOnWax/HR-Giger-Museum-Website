d25087aa58df2d3fabf5b568c8fda080782a8292
AlexOnWax <alexandre.barbet@hotmail.com>
2022-07-09 11:28:19 +0200

    ajout de quelques commenataires

--- JS/script_connection_dashboard_ajax.js
+++ JS/script_connection_dashboard_ajax.js
@@ -1,54 +1,51 @@
 //---Validation formulaire---
 
-const formulaire = document.querySelector('form');
-const inputs = document.querySelectorAll('.input');
-const formBtn =document.querySelector('form > button');
-const errors = document.querySelectorAll('.invisible');
+const formulaire = document.querySelector("form");
+const inputs = document.querySelectorAll(".input");
+const formBtn = document.querySelector("form > button");
+const errors = document.querySelectorAll(".invisible");
 
+// Validation du formulaire de connexion au Dashboard
 function formValidation() {
-let i=-1;
-inputs.forEach((input) => {
-i++;
-input.addEventListener("input",() => {
-  });
+  let i = -1;
+  inputs.forEach((input) => {
+    i++;
+    input.addEventListener("input", () => {});
 
-  if (input.validity.valueMissing){
-  const message = document.createTextNode(`${input.id} cannot be empty`);
-  errors[i].replaceChildren(message);
-  errors[i].classList.add("help");
-  input.classList.add("error");
+    if (input.validity.valueMissing) {
+      const message = document.createTextNode(`${input.id} cannot be empty`);
+      errors[i].replaceChildren(message);
+      errors[i].classList.add("help");
+      input.classList.add("error");
+    } else if (input.validity.typeMismatch) {
+      const message = document.createTextNode(`Invalide ${input.id}`);
+      errors[i].replaceChildren(message);
+      errors[i].classList.add("help");
+      input.classList.add("error");
+    } else {
+      const message = document.createTextNode("");
+      errors[i].replaceChildren(message);
+      errors[i].classList.add("help");
+      input.classList.remove("error");
 
-  }else if (input.validity.typeMismatch){
-    const message = document.createTextNode(`Invalide ${input.id}`);
-    errors[i].replaceChildren(message)
-    errors[i].classList.add("help");
-    input.classList.add("error");
-  }else{
-    const message= document.createTextNode("");
-    errors[i].replaceChildren(message);
-    errors[i].classList.add("help");
-    input.classList.remove("error");
-
-    const loginData = new FormData(formulaire);
-    fetch('verification_connexion_dashboard.php', {
-        method: 'POST',
-        body: loginData,})
-   .then(response=>response.json())
-   .then(response=>{
-    
-    if(response[0]==false){
-      console.log("ERROR");
-    }else {
-      window.location.assign("page_dashboard_mailing_list.php");
+      const loginData = new FormData(formulaire);
+      //Fetch PHP 
+      fetch("verification_connexion_dashboard.php", {
+        method: "POST",
+        body: loginData,
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          if (response[0] == false) {
+            console.log("ERROR");
+          } else {// permet la redirection si ok
+            window.location.assign("page_dashboard_mailing_list.php");
+          }
+        });
     }
-   
-  })
-  
-  }
-})
+  });
 }
-formulaire.addEventListener('submit', function (event){
-event.preventDefault();
-formValidation();
-
-})
+formulaire.addEventListener("submit", function (event) {
+  event.preventDefault();
+  formValidation();
+});
--- JS/script_inscription_newsletter_ajax.js
+++ JS/script_inscription_newsletter_ajax.js
@@ -1,49 +1,38 @@
-const formulaireNewsletter = document.querySelector('form');
-const email = document.querySelector('#email');
-const button = document.querySelector('form > button');
-const input = document.querySelector('.input');
-const error = document.querySelector('.invisible');
-const goodMessage = document.querySelector('.invisible_good');
+const formulaireNewsletter = document.querySelector("form");
+const email = document.querySelector("#email");
+const button = document.querySelector("form > button");
+const input = document.querySelector(".input");
+const error = document.querySelector(".invisible");
+const goodMessage = document.querySelector(".invisible_good");
 //---Validation formulaire---
 function formValidation() {
   goodMessage.classList.remove("help");
   error.classList.remove("help");
-  if (input.validity.valueMissing){
-    
-  const message = document.createTextNode(`Email cannot be empty`);
-  error.replaceChildren(message);
-  error.classList.add("help");
-  input.classList.add("error");
-
-  }else if (input.validity.typeMismatch){
-   
+  if (input.validity.valueMissing) {
+    const message = document.createTextNode(`Email cannot be empty`);
+    error.replaceChildren(message);
+    error.classList.add("help");
+    input.classList.add("error");
+  } else if (input.validity.typeMismatch) {
     const message = document.createTextNode(`This is not a valid email`);
     error.replaceChildren(message);
     error.classList.add("help");
     input.classList.add("error");
-  }else{
-    const message= document.createTextNode("Thanks you for the subscription");
-    // goodMessage.replaceChildren(message);
-    // goodMessage.classList.add("help");
+  } else {
+    const message = document.createTextNode("Thanks you for the subscription");
     goodMessage.replaceChildren(message);
     goodMessage.classList.add("help");
     input.classList.add("good");
-   
- const formData = new FormData(formulaireNewsletter);
-fetch('verification_formulaire_inscription_newletter.php', {
-    method: 'POST',
-    body: formData,
-}).then((response)=>response.text());
+
+    const formData = new FormData(formulaireNewsletter);
+    fetch("verification_formulaire_inscription_newletter.php", {
+      method: "POST",
+      body: formData,
+    }).then((response) => response.text());
   }
 }
-formulaireNewsletter.addEventListener('submit', function (e){
-e.preventDefault();
-formValidation();
-input.value="";
+formulaireNewsletter.addEventListener("submit", function (e) {
+  e.preventDefault();
+  formValidation();
+  input.value = "";
 });
-
-
-
-
-
-
