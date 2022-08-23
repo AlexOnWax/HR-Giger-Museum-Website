8498fc4a385561c4ea87af7f96abc017fcf43fdf
AlexOnWax <alexandre.barbet@hotmail.com>
2022-08-23 15:45:35 +0200

    problème validation mail règlé

--- php/verification_formulaire_inscription_newletter.php
+++ php/verification_formulaire_inscription_newletter.php
@@ -7,26 +7,21 @@ if(!empty($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)
 
     try {
         $dbnl = new PDO('mysql:host=' . $servername . ';dbname=' . $dbName, $user, $pass);
-//        $dbnl->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dbnl->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sql = $dbnl->prepare("INSERT INTO Mail(email) VALUES(:email)");
         $sql->bindParam(':email', $email, PDO::PARAM_STR);//Ajoute securité précise le type de donné attendu
         $sql->execute();
     } catch (PDOException $e) {
-        echo "Erreur :" . $e;
-            header('error:' .$e);
+        //echo "Erreur :" . $e;
+            header('error:' .$e->getCode());
+echo json_encode($e->getCode());
     }
 
-    if (!$sql->execute()) {
-        $errorResp = $sql->errorCode();
-        header('error:' .$errorResp);
-
-    } else {
-        $errorResp = 1;
-        header('error:' .$errorResp);
-    }
+ 
 
 }else{// permet la redirection si on accede directement a la page depuis la barre d'adresse
 
     header("Location: index.html");
 
 }
+
